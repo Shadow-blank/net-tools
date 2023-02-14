a9a46d09ba7c9235f6c6b0ceb032b05228b82839
qu <qu19950523@vip.qq.com>
2023-02-14 16:02:54 +0800

    修改描述

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 // ==UserScript==
 // @name         冲浪助手
 // @namespace    https://github.com/Shadow-blank/net-tools
-// @version      0.1
+// @version      0.1.1
 // @description  让web页面更加容易浏览、操作
 // @author       Shadow-blank
-// @match        https://m.weibo.cn/status/*
+// @match        *://m.weibo.cn/status/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=weibo.cn
 // @grant        none
 // @license      MIT
 // ==/UserScript==
 
-(function() {
-  'use strict';
+(function () {
+  'use strict'
 
-  const weiboMFn = () => {
-    setTimeout(()=>{
+  // 微博移动端自动跳转到PC
+  const weiboMJump2PC = () => {
+    setTimeout(() => {
       const userId = document.querySelector('.m-img-box').href.split('/')[4]
-      const id =  location.pathname.split('/')[2]
+      const id = location.pathname.split('/')[2]
       location.href = `https://weibo.com/${userId}/${id}`
     }, 500)
   }
 
   const host = location.host
 
-  const weiboHostM = 'm.weibo.cn'
-
+  const WEIBO_M = 'm.weibo.cn'
 
   switch (host) {
-    case weiboHostM :
-      weiboMFn()
-    break
+    case WEIBO_M :
+      weiboMJump2PC()
+      break
   }
 
-})();
+})()
