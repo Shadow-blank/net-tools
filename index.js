228cedf33a7133b41b48e9930881a07a02b50401
qu <qu19950523@vip.qq.com>
2023-02-14 19:56:11 +0800

    add bilibili

--- index.js
+++ index.js
@@ -9,6 +9,8 @@
 // @match        *://2.comicat.net/*
 // @match        *://3.comicat.net/*
 // @match        *://dick.xfani.com/*
+// @match        *://live.bilibili.com/*
+// @match        *://www.bilibili.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=weibo.cn
 // @grant        none
 // @license      MIT
@@ -56,13 +58,52 @@
     R("selectstart");
   }
 
+  // bilibili去除首页轮播图 只保留番剧和推荐
+  const bilibiliRomoveBanner = () => {
+    // 取消bilibili默认样式
+    const style = document.createElement('style')
+    style.innerHTML = `.bili-video-card.is-rcmd {display: block!important; }`
+    document.querySelector('body').appendChild(style)
+    // 去除轮播图
+    setTimeout(() => {
+      const removeClassList = ['recommended-swipe', 'eva-extension-area', 'battle-area', 'live-card-list', 'video-card-list']
+      document.querySelectorAll('.bili-grid').forEach((item, index) => {
+        index > 7 && item.remove()
+      })
+      removeClassList.forEach(item => {
+        document.querySelector(`.${item}`).remove()
+      })
+    }, 1000)
+  }
+
+  // 直播页面去掉广告 只显示直播
+  const biliLiveRmoveOther = () => {
+    let timer = ''
+    // 推广直播页面进入原始页面
+    timer = setInterval(() => {
+      const iframe = document.querySelector('#app iframe')
+      if (iframe?.src.includes('/blanc/')) {
+        clearInterval(timer)
+        location.href = iframe.src.split('?')[0]
+      }
+    }, 100)
+    // 将直播画面居中显示
+    document.querySelector('.live-room-app.p-relative').style.overflow = 'hidden'
+    document.querySelector('main').style.padding = '0px'
+    document.querySelector('.player-ctnr.left-container.p-relative.z-player-ctnr').style = 'width: 100%; margin: 10px auto'
+    document.querySelector('#iframe-popup-area').remove()
+    document.querySelector('section').style.margin = '0px'
+  }
+
   const host = location.host
 
   const hostMap = {
     'm.weibo.cn': weiboMJump2PC,
     '2.comicat.net': comicatMoveDown,
     '3.comicat.net': comicatMoveDown,
-    "dick.xfani.com": xfaniRventMenu
+    "dick.xfani.com": xfaniRventMenu,
+    "www.bilibili.com": bilibiliRomoveBanner,
+    "live.bilibili.com": biliLiveRmoveOther
   }
 
   hostMap[host] && hostMap[host]()
