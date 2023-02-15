54d63e45354761d5e3e86a1bf4300b29e4f44696
qu <qu19950523@vip.qq.com>
2023-02-15 09:56:14 +0800

    优化逻辑

--- index.js
+++ index.js
@@ -36,44 +36,27 @@
 
   // 稀饭取消右键限制
   const xfaniRventMenu = () => {
-
-    function R(type) {
-      const onType = "on" + type;
-      window.addEventListener(type, function (e) {
-        for (let n = e.originalTarget; n; n = n.parentNode) {
-          n[onType] = null;
-        }
-      }, true);
-      window[onType] = null;
-      document[onType] = null;
-      if (document.body) {
-        document.body[onType] = null;
-      }
+    ['contextmenu', 'click', 'mousedown', 'mouseup', 'selectstart'].forEach(item => clearEvent(item))
+    function clearEvent (type) {
+      const onType = 'on' + type
+      document[onType] = null
+      document.body && (document.body[onType] = null)
     }
-
-    R("contextmenu");
-    R("click");
-    R("mousedown");
-    R("mouseup");
-    R("selectstart");
   }
 
   // bilibili去除首页轮播图 只保留番剧和推荐
   const bilibiliRomoveBanner = () => {
     // 取消bilibili默认样式
     const style = document.createElement('style')
-    style.innerHTML = `.bili-video-card.is-rcmd {display: block!important; }`
+    style.innerHTML = `
+     .bili-video-card.is-rcmd{
+        display: block!important; 
+     }
+     .recommended-swipe, .eva-extension-area, .battle-area, .live-card-list, .live-rank-conent, .video-card-list, .bili-grid:nth-child(n+10), .eva-banner{
+        display: none!important; 
+     }`
     document.querySelector('body').appendChild(style)
     // 去除轮播图
-    setTimeout(() => {
-      const removeClassList = ['recommended-swipe', 'eva-extension-area', 'battle-area', 'live-card-list', 'video-card-list']
-      document.querySelectorAll('.bili-grid').forEach((item, index) => {
-        index > 7 && item.remove()
-      })
-      removeClassList.forEach(item => {
-        document.querySelector(`.${item}`).remove()
-      })
-    }, 1000)
   }
 
   // 直播页面去掉广告 只显示直播
@@ -101,9 +84,9 @@
     'm.weibo.cn': weiboMJump2PC,
     '2.comicat.net': comicatMoveDown,
     '3.comicat.net': comicatMoveDown,
-    "dick.xfani.com": xfaniRventMenu,
-    "www.bilibili.com": bilibiliRomoveBanner,
-    "live.bilibili.com": biliLiveRmoveOther
+    'dick.xfani.com': xfaniRventMenu,
+    'www.bilibili.com': bilibiliRomoveBanner,
+    'live.bilibili.com': biliLiveRmoveOther
   }
 
   hostMap[host] && hostMap[host]()
